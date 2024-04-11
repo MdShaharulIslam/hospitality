88b720c50efd4cb0990508b86f1de2f332822745
MdShaharulIslam <shaharulislam025@gmail.com>
2024-04-11 23:04:05 +0600

    banner section compelte

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -5,24 +5,20 @@ import lodge from '/public/lodge.jpeg'
 
 const Banner = () => {
     return (
-
-        
-        <div className="carousel  w-full h-[600px]  ">
+              <div className="carousel  w-full h-[600px]  ">
           
         <div id="slide1" className="carousel-item relative w-full  ">
           <img src={hotel} className="w-full rounded-lg" />
           <div className="hero-content text-center text-neutral-content absolute -mx-1/2 animate__animated animate__fadeIn">
   <div className="max-w-md justify-start">
     <h1 className="mb-5 text-5xl font-bold text-purple-400">Hello.. <br /><span>We are Providing best Services</span> </h1>
-   
-   
-  </div>
+      </div>
 </div>
-
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">❮</a> 
             <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
+          
         </div> 
         <div id="slide2" className="carousel-item relative w-full">
           <img src={motel} className="w-full rounded-lg" />
